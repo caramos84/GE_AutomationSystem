d510251b298b79cce3d346f478c095132a98a7fe
Carlos Andres Ramos <ca.ramosvelasquez@gmail.com>
2025-11-27 18:00:27 -0500

    Fix DB config, add table creation, working uploads

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 from app.routes import uploads
 
+from app.db import Base, engine
+from app import models
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
 
 @app.get("/health")
